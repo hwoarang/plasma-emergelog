3fa44a561b451bc743b9323be3019425af8094e3
Markos Chandras <hwoarang@gentoo.org>
2009-04-23 13:20:15 +0100

    Fix regexp

--- plasma-emergelog.cpp
+++ plasma-emergelog.cpp
@@ -163,7 +163,8 @@ void emergelog::process_data(){
 		tmp=list.at(i);
 		tmp.replace(QRegExp("^\\d{1,10}:\\s{1,3}"), " ");
 		tmp.replace(QRegExp("^\\s{1,3}S"),"  S");
-		tmp.replace(QRegExp("::.*"), " Finished :-) ");
+		tmp.replace(QRegExp("::/.*"), " ");
+		tmp.replace(QRegExp(":::.*")," Finished");
 		tmp.replace(QRegExp("\\*\\*\\* "), " ");
 		tmp.replace(QRegExp(">>> "), " ");
 		tmp.replace(QRegExp("=== "),"  ");
